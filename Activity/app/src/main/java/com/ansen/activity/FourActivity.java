07d34dd046243a3cca514299169f8de8d8c4abcf
who7708 <who7708@qq.com>
2022-12-12 23:27:25 +0800

    migrate to AndroidX

--- Activity/app/src/main/java/com/ansen/activity/FourActivity.java
+++ Activity/app/src/main/java/com/ansen/activity/FourActivity.java
@@ -2,10 +2,9 @@ package com.ansen.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
 
 /**
  * Created by  ansen
